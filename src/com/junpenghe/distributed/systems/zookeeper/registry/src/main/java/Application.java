9184a1b1dbc57c6a5c4b0d5c5be3e4992daa08bd
Junpeng He <hejunpeng2012@hotmail.com>
2020-11-08 18:50:09 +0800

    ServiceRegistry

---
+++ src/com/junpenghe/distributed/systems/zookeeper/registry/src/main/java/Application.java
@@ -0,0 +1,51 @@
+import cluster.management.LeaderElection;
+import cluster.management.OnElectionAction;
+import cluster.management.OnElectionCallback;
+import cluster.management.ServiceRegistry;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+public class Application implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private static final int DEFAULT_PORT = 8083;
+    private ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws Exception{
+        int currentServerPort = args.length == 1 ? Integer.parseInt(args[0]) : DEFAULT_PORT;
+        Application application = new Application();
+        ZooKeeper zooKeeper = application.connectToZookeeper();
+        ServiceRegistry serviceRegistry = new ServiceRegistry(zooKeeper);
+        OnElectionCallback onElectionCallback = new OnElectionAction(serviceRegistry, currentServerPort);
+        LeaderElection leaderElection = new LeaderElection(zooKeeper, onElectionCallback);
+        leaderElection.volunteerForLeadership();
+        leaderElection.reelectLeader();
+
+        application.run();
+        application.close();
+        System.out.println("Disconnected from Zookeeper, exiting application");
+    }
+
+    public ZooKeeper connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+        return zooKeeper;
+    }
+
+    private void run() throws InterruptedException {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    private void close() throws InterruptedException {
+        this.zooKeeper.close();
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+
+    }
+}
