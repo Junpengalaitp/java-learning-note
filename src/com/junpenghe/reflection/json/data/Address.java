60faf983e81c358b98571758a9df83a891748798
Junpeng.He <hejunpeng2012@hotmail.com>
2020-11-13 12:43:03 +0800

    reflection

---
+++ src/com/junpenghe/reflection/json/data/Address.java
@@ -0,0 +1,14 @@
+package com.junpenghe.reflection.json.data;
+
+/**
+ * @author Junpeng He
+ */
+public class Address {
+    private final String street;
+    private final short apartment;
+
+    public Address(String street, short apartment) {
+        this.street = street;
+        this.apartment = apartment;
+    }
+}
