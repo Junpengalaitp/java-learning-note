d95107560bb04518a0d5058da704474792a1681d
Junpeng He <hejunpeng2012@hotmail.com>
2021-04-11 20:59:12 +0800

    Spark

--- src/com/junpenghe/spark/src/main/java/com/junpenghe/Main.java
+++ src/com/junpenghe/spark/src/main/java/com/junpenghe/Main.java
@@ -3,21 +3,23 @@ package com.junpenghe;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Double> inputData = new ArrayList<>();
-        inputData.add(35.5);
-        inputData.add(12.49943);
-        inputData.add(90.32);
-        inputData.add(20.32);
+        List<String> inputData = Arrays.asList(
+                "WARN: Tuesday 4 September 0405",
+                "ERROR: Tuesday 4 September 0409",
+                "FATAL: Wednesday 5 September 1632",
+                "ERROR: Friday 5 September 1854",
+                "WARN: Saturday 8 September 1942"
+        );
 
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
@@ -26,7 +28,15 @@ public class Main {
                 .setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<Double> myRdd = sc.parallelize(inputData);
+//        sc.parallelize(inputData)
+//                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+//                .reduceByKey(Long::sum)
+//                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+
+        sc.parallelize(inputData)
+                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+                .groupByKey()
+                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2.spliterator().getExactSizeIfKnown() + " instances"));
 
         sc.close();
 
