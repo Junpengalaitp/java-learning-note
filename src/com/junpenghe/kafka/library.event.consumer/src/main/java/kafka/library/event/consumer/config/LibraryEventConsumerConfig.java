ab4f0f2b49b4bf4b315e75a890effe2f747323c0
Junpeng He <hejunpeng2012@hotmail.com>
2021-04-13 20:55:45 +0800

    Kafka error handling and recovery

--- src/com/junpenghe/kafka/library.event.consumer/src/main/java/kafka/library/event/consumer/config/LibraryEventConsumerConfig.java
+++ src/com/junpenghe/kafka/library.event.consumer/src/main/java/kafka/library/event/consumer/config/LibraryEventConsumerConfig.java
@@ -1,17 +1,28 @@
 package kafka.library.event.consumer.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Junpeng He
  */
+@Slf4j
 @Configuration
 @EnableKafka
 public class LibraryEventConsumerConfig {
@@ -24,7 +35,37 @@ public class LibraryEventConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.BATCH);
         factory.setConcurrency(3);
+        factory.setRetryTemplate(retryTemplate());
+        factory.setRecoveryCallback(context -> {
+            if (context.getLastThrowable().getCause() instanceof RecoverableDataAccessException) {
+                log.info("Inside the recoverable exception");
+                Arrays.stream(context.attributeNames()).forEach(name -> {
+                    log.info("Attribute name is: {}", name);
+                    log.info("Attribute value is: {}", context.getAttribute(name));
+                });
+            } else {
+                log.info("Inside the non-recoverable exception");
+                throw new RuntimeException(context.getLastThrowable().getMessage());
+            }
+            return null;
+        });
         configurer.configure(factory, kafkaConsumerFactory);
         return factory;
+    }
+
+    private RetryTemplate retryTemplate() {
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(1000);
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(simpleRetryPolice());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        return retryTemplate;
+    }
+
+    private RetryPolicy simpleRetryPolice() {
+        Map<Class<? extends Throwable>, Boolean> exceptionsMap = new HashMap<>();
+        exceptionsMap.put(Exception.class, true);
+        exceptionsMap.put(RecoverableDataAccessException.class, false);
+        return new SimpleRetryPolicy(3, exceptionsMap, true);
     }
 }
