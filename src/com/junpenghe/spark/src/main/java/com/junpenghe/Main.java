97723dad5ea65d5500431b6c651b1ed1507508e6
Junpeng He <hejunpeng2012@hotmail.com>
2021-04-16 20:15:49 +0800

    More on spark

--- src/com/junpenghe/spark/src/main/java/com/junpenghe/Main.java
+++ src/com/junpenghe/spark/src/main/java/com/junpenghe/Main.java
@@ -1,26 +1,22 @@
 package com.junpenghe;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> inputData = Arrays.asList(
-                "WARN: Tuesday 4 September 0405",
-                "ERROR: Tuesday 4 September 0409",
-                "FATAL: Wednesday 5 September 1632",
-                "ERROR: Friday 5 September 1854",
-                "WARN: Saturday 8 September 1942"
-        );
-
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf conf = new SparkConf()
@@ -28,15 +24,19 @@ public class Main {
                 .setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-//        sc.parallelize(inputData)
-//                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
-//                .reduceByKey(Long::sum)
-//                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+        JavaRDD<String> initRDD = sc.textFile("src/com/junpenghe/spark/src/main/resources/subtitles/input.txt");
+        JavaRDD<String> lettersOnlyRDD = initRDD.map(sentence -> sentence.replaceAll("[^a-zA-Z\\s]", "").toLowerCase(Locale.ROOT));
+        JavaRDD<String> noBlankRDD = lettersOnlyRDD.filter(sentence -> !sentence.trim().isEmpty());
+        JavaRDD<String> words = noBlankRDD.flatMap(sentence -> Arrays.stream(sentence.split(" ")).iterator());
+        JavaRDD<String> validWords = words.filter(StringUtils::isNoneEmpty);
+        JavaRDD<String> justInterestWords = validWords.filter(Util::isNotBoring);
+        JavaPairRDD<String, Integer> stringIntegerJavaPairRDD = justInterestWords.mapToPair(word -> new Tuple2<>(word, 1));
+        JavaPairRDD<String, Integer> totals = stringIntegerJavaPairRDD.reduceByKey(Integer::sum);
+        JavaPairRDD<Integer, String> switched = totals.mapToPair(tuple -> new Tuple2<>(tuple._2, tuple._1));
+        JavaPairRDD<Integer, String> sorted = switched.sortByKey(false);
+        List<Tuple2<Integer, String>> result = sorted.take(10000);
+        result.forEach(System.out::println);
 
-        sc.parallelize(inputData)
-                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
-                .groupByKey()
-                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2.spliterator().getExactSizeIfKnown() + " instances"));
 
         sc.close();
 
